2b778fb0bb471aeac79f08928a0c0c599a485004
ehajdeu <elena.hajdeu-chicaros@endava.com>
2022-10-07 18:04:08 +0300

    Third Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("This is my Git Test 1!!");
+        System.out.println("This is my Git Test 2!!");
     }
 }
